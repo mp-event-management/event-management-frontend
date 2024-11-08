4cdd2524667060d037b66413220cb174a8da413b
mdedealf <dede.alfaruqq@gmail.com>
2024-11-08 18:55:44 +0700

    test: testing

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -5,6 +5,7 @@ const EventPage: FC = () => {
     <div>
       <h1>Event Page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ea, quidem officiis quos commodi quibusdam distinctio quis aliquid corporis in ipsam enim iure est. Magni fugit illum eum qui vel!</p>
+      <p>Test commit</p>
     </div>
   );
 };
