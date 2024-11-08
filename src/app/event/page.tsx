26313fa514684bdbad04aaa7c76e21ed589be45b
mdedealf <dede.alfaruqq@gmail.com>
2024-11-08 13:03:50 +0700

    chore: adding some testing code

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -4,6 +4,7 @@ const EventPage: FC = () => {
   return (
     <div>
       <h1>Event Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, ea, quidem officiis quos commodi quibusdam distinctio quis aliquid corporis in ipsam enim iure est. Magni fugit illum eum qui vel!</p>
     </div>
   );
 };
