e787cf5f6e33bdeb5d83d5de8a41eb32d71bedae
dvcsr <4th3sh0ws@gmail.com>
2024-12-09 12:59:15 +0700

    add register page router

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+import { FC } from "react";
+
+const RegisterPage: FC = () => {
+  return (
+    <div className="h-[calc(100vh-180px)]">
+      <Container>This is register page</Container>
+    </div>
+  );
+};
+
+export default RegisterPage;
