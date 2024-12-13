9eed78a3ef8d93a6a891f2b498a231b09e948ca2
mdedealf <dede.alfaruqq@gmail.com>
2024-12-13 22:34:12 +0700

    add : add event details type

--- src/app/(nosearch)/events/[id]/reviews/page.tsx
+++ src/app/(nosearch)/events/[id]/reviews/page.tsx
@@ -4,35 +4,17 @@ import { getAllEventReviews, getEventDetail } from "@/app/api/api";
 import Container from "@/components/Container";
 import EmptyState from "@/components/EmptyState";
 import { Button } from "@/components/ui/Button";
+import { Event } from "@/types/eventDetails";
 import { EventReviews } from "@/types/eventReviews";
-import { Category, City, Promotions, UserOrganizer } from "@/types/getEvents";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-type Event = {
-  eventId: number;
-  userOrganizer: UserOrganizer;
-  title: string;
-  description: string;
-  category: Category;
-  eventImagesUrl: string;
-  startDate: Date; // ISO 8601 date string
-  endDate: Date; // ISO 8601 date string
-  ticketPrice: number;
-  totalTicket: number;
-  availableTicket: number;
-  eventStatus: string;
-  city: City;
-  address: string;
-  promotions: Promotions[];
-};
-
 const EventReviewsPage = () => {
   const [reviews, setReviews] = useState<EventReviews[]>([]);
-  const [eventDetail, setEventDetail] = useState<Event>();
+  const [event, setEvent] = useState<Event>();
   const [currentPage, setCurrentpage] = useState(0);
   const [totalPage, setTotalPages] = useState(0);
 
@@ -55,7 +37,7 @@ const EventReviewsPage = () => {
 
   useEffect(() => {
     setReviews(data?.eventReviews);
-    setEventDetail(eventDetails);
+    setEvent(eventDetails);
     setTotalPages(data?.totalPages);
   }, [data, reviews, eventDetails]);
 
@@ -74,7 +56,7 @@ const EventReviewsPage = () => {
           Rating and Reviews
         </h1>
         <h3 className="text-xl flex gap-4 items-center lg:text-2xl font-bold text-start">
-          {eventDetail?.title}
+          {event?.title}
           <p>-</p>
           {reviews?.length <= 0 ? (
             <p className="text-lg font-normal">No reviews</p>
