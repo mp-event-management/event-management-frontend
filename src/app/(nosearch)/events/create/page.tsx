0b3ac8664238139837742d43b894a6cf82a79500
mdedealf <dede.alfaruqq@gmail.com>
2024-12-14 14:40:47 +0700

    add : add suspense component wrapper create page

--- src/app/(nosearch)/events/create/page.tsx
+++ src/app/(nosearch)/events/create/page.tsx
@@ -2,29 +2,37 @@
 
 import EventForm from "@/components/form/EventForm";
 import { useSession } from "next-auth/react";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 
 const CreateEventPage: FC = () => {
   const { data: session } = useSession();
   const organizerId = session?.user.id;
 
   return (
-    <section className="w-full min-h-[calc(100vh-95px)] lg:max-w-xl lg:mx-auto lg:my-16 mt-6">
-      <h1 className="text-2xl lg:text-4xl font-extrabold text-center">
-        Create your new event
-      </h1>
+    <Suspense
+      fallback={
+        <div className="w-full flex items-center justify-center min-h-[calc(100vh-95px)]">
+          Loading ...
+        </div>
+      }
+    >
+      <section className="w-full min-h-[calc(100vh-95px)] lg:max-w-xl lg:mx-auto lg:my-16 mt-6">
+        <h1 className="text-2xl lg:text-4xl font-extrabold text-center">
+          Create your new event
+        </h1>
 
-      <div
-        className="flex flex-col my-12 px-6
+        <div
+          className="flex flex-col my-12 px-6
         "
-      >
-        <EventForm
-          type="Create"
-          organizerId={Number(organizerId)}
-          accessToken={session?.accessToken}
-        />
-      </div>
-    </section>
+        >
+          <EventForm
+            type="Create"
+            organizerId={Number(organizerId)}
+            accessToken={session?.accessToken}
+          />
+        </div>
+      </section>
+    </Suspense>
   );
 };
 
